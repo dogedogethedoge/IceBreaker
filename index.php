7ecf81ccdfd70dd5353cdfcbd78e65a30873e733
dogedogethedoge <zehaosui@yahoo.com>
2018-12-09 20:18:27 -0500

    Update index.php

--- index.php
+++ index.php
@@ -42,13 +42,13 @@
 
         <h3>Play game in pop up window:<h3>
         <form id="gameSettings" class="well"></form>
-        <iframe src="games/Icebreaker/iframe.html" class="game" width="800" height="800"></iframe>
+        <iframe src="games/IceBreaker/iframe.html" class="game" width="800" height="800"></iframe>
 
     </article>
     <?php include $base."footer.php"; ?>
 </div>
 <script type="text/javascript">
-    newWindowBtn(1200,1200,"games/Icebreaker/iframe.html", []);
+    newWindowBtn(1200,1200,"games/IceBreaker/iframe.html", []);
 </script>
 </body>
 </html>
