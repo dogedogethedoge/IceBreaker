0929b00181838e6142f215df071a6492e80ce5a4
Isaac <isaachaberman@gmail.com>
2018-12-16 22:00:35 -0500

    Updated Instructions

--- index.php
+++ index.php
@@ -31,11 +31,12 @@
         <div id="gameDesc" class="jumbotron">
             <div>
                 <em>
-                Rules <br> <br />
-				- Player 1 will choose a 2x2 set of blocks for the polar bears. <br> <br />
-				- Players will alternate turns choosing where to hit the blocks that do not contain bears each turn, the intersections may be hit as well. <br> <br />
-				- The game continues until a player knocks all four polar bears into the water.  They can hold each up until if only a few of them are knocked in. <br> <br />
-				- Ice blocks will only stay up if it is in either a complete row or complete column. <br> <br />
+                - Player 1 will choose a 2x2 set of blocks for the polar bears. <br> <br />
+                - Players will alternate turns choosing where to hit the blocks the bear is not resting on each turn, the intersections may be hit as well. <br> <br />
+                - The game continues until a player knocks all four polar bears into the water.  They can hold each up until if only a few of them are knocked in. <br> <br />
+                - Ice blocks will only stay up if it is in either a complete row or complete column. <br> <br />
+                - Each player can click Confirm to finish their term, or double click their choice <br> <br />
+                - Players can also turn on a hint mode, to show which blocks will fall with their current selection. <br> <br />
                 </em>
                 Good Luck!     
             </div>
